9b8dc8e94033b9cb6046682091a2dfb79977f9ea
vargalaszlo87 <varga.laszlo.drz@gmail.com>
2023-07-06 14:39:41 +0200

    First upload.

---
+++ vector.min.int.c
@@ -0,0 +1,9 @@
+// min value of int vector
+int min(int *a, int n) {
+	int temp = a[0], i = 0;
+	while (++i < n)
+		temp = (a[i] < temp) ? a[i] : temp;
+	return temp;
+}
+
+// use it: int smallest = min(vector, 18);
