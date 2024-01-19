dec1b55fa971c1bcd035686eab0922d695ceb432
Laszlo Varga <varga.laszlo.drz@gmail.com>
2024-01-19 22:22:05 +0100

    Update vector.maxIndex.double.c

--- vector.maxIndex.double.c
+++ vector.maxIndex.double.c
@@ -1,13 +1,13 @@
 // max value's index of (double) vector
 int maxIndex(double *a, double n) {
-  double temp = a[0];
-  int i = -1, j = 0;
-  while (++i < n) {
-    if (a[i] > temp)
-      temp = a[i],
-      j = i;
+	double temp = a[0];
+	int i = -1, j = 0;
+	while (++i < n) {
+		if (a[i] > temp)
+			temp = a[i],
+			j = i;
 	}
-	return j;
+return j;
 }
 
-// usage: int index = max(vector, 18);
+// usage: int index = maxIndex(vector, 18);
